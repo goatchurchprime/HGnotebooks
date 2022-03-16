4dc66b6b5a2421988722c2d7011b73e7ce2ef444
Julian Todd <julian@goatchurch.org.uk>
2022-03-16 11:14:37 +0000

    wingflatten update

--- wingflattening/freecad_macro_work/winggengroup.py
+++ wingflattening/freecad_macro_work/winggengroup.py
@@ -72,22 +72,53 @@ def convertDS(pts, ds, seam, chord):
 		pl = p
 	return aerof,dsp
 
-mwires = [ ]
+for i in range(len(secs)+100):
+	wi = doc.findObjects(Name="sec%d"%i)
+	if wi:
+		doc.removeObject(wi[0])
+	else:
+		break
+
+secbsplineedges = [ ]
 for i in range(len(secs)):
 	points, dsp = convertDS(pts, doubleSurfs[i], seamDSs[i], chords[i])
 	secplacement = App.Placement(App.Vector(sweeps[i], secs[i], dihedrals[i]), 
 									App.Rotation(App.Vector(0,1,0), twists[i]), App.Vector(0,0,0))
+	wi = doc.addObject("Part::Feature", "sec%d"%i)
+	wi.Shape = Part.makePolygon(points)
+	wi.Placement = secplacement
+	secbspline = Part.BSplineCurve()
+	secbspline.approximate(points, Parameters=range(len(points)))
+	secbsplineE = Part.Edge(secbspline)
+	secbsplineE.Placement = secplacement
+	secbsplineedges.append(secbsplineE)
 	
-	#ppoints = [secplacement*p  for p in points]
-	#wpoints = Part.makePolygon(ppoints)
-	#mwires.append(wpoints)
-	wp = Part.makePolygon(points)
-	wp.Placement = secplacement
-	mwires.append(wp)
+wingloft = doc.findObjects(Name="wingloft")
+wingloft = wingloft[0] if wingloft else doc.addObject("Part::Feature", "wingloft")
+wingloft.Shape = Part.makeLoft(secbsplineedges)
 
-m = Part.makeLoft(mwires)
-wlF = doc.findObjects(Name="wingloft")
-wl = wlF[0] if wlF else doc.addObject("Part::Feature", "wingloft")
-wl.Shape = m
 doc.recompute()
+
+# wlf = doc.wingloft.Shape.Faces[0]
+# this is interpolated 0->1 in both parameters
+
+# We can do Part.Vertex(x,y,z).distToShape(doc.wingloft) and get a triple of 
+# (distance, [(vertex, closestpointinwingloft)], [uvparameterspaceinfo])
+
+# the lofted surface is a shell shape with Faces for each of the pairs of 90 nodes. 
+# each face is u in [i,i+1] and v in [0,1]
+# face.valueAt(u, v)
+# face.getUVNodes()
+# face.isPartOfDomain(u, v)
+
+# obj is a Wire
+# b = [ v.Point  for v in obj.Shape.Vertexes]
+# splx = [ p.x  for p in b ]
+# splz = [ p.x  for p in b ]
+
+#V=App.Vector
+#poles=[V(-10,-10),V(10,-10),V(10,10),V(-10,10)]
+#n=Part.BSplineCurve()
+#Part.show(n.toShape())
+
 
