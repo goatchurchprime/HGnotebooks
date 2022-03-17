008863d49ae787964b53b43d427561984b6b0087
Julian Todd <julian@goatchurch.org.uk>
2022-03-17 13:30:23 +0000

    move seval for the wing out into its own module

--- wingflattening/freecad_macro_work/p7generatemesh.py
+++ wingflattening/freecad_macro_work/p7generatemesh.py
@@ -8,6 +8,9 @@ import math, os, csv
 import numpy
 from FreeCAD import Vector, Rotation
 
+from p7modules.p7wingeval import urange, vrange, seval
+print("Rangess", urange, vrange)
+
 doc = App.ActiveDocument
 	
 def getemptyobject(doc, objtype, objname):
@@ -15,25 +18,6 @@ def getemptyobject(doc, objtype, objname):
 		doc.removeObject(objname)
 		doc.recompute()
 	return doc.addObject(objtype, objname)
-
-# parametric definition functions for the wingshape
-def paramintconv(u, uvals):
-	j0, j1 = 0, len(uvals)-1
-	while j1 - j0 >= 2:
-		j = (j1 + j0)//2
-		if u <= uvals[j]:
-			j1 = j
-		else:
-			j0 = j
-	return j0 + (u-uvals[j0])/(uvals[j1]-uvals[j0])
-
-def seval(u, v):
-	uc = paramintconv(u, uvals)
-	i = max(0, min(len(uvals)-2, int(uc)))
-	m = uc - i
-	p0 = sections[i].Shape.valueAt(v)
-	p1 = sections[i+1].Shape.valueAt(v)
-	return p0*(1-m) + p1*m
 
 def genfacets(usteps, vsteps):
 	prevrow = [ ]
@@ -49,20 +33,6 @@ def genfacets(usteps, vsteps):
 		prevrow = row
 	return facets
 	
-
-# Fetch the sections from the folder and find the parameter ranges
-sections = doc.getObject("SectionGroup").OutList
-
-leadingedgepoints = [ s.Shape.valueAt(0)  for s in sections ]
-leadingedgelengths = [ 0.0 ]
-for i in range(len(leadingedgepoints)-1):
-	leadingedgelengths.append(leadingedgelengths[-1] + (leadingedgepoints[i+1] - leadingedgepoints[i]).Length)
-uvals = leadingedgelengths
-
-urange = [0, uvals[-1]]
-vrange = [sections[0].Shape.FirstParameter, sections[0].Shape.LastParameter]
-print("Ranges", urange, vrange)
-
 # generate and plot the meshed object
 mesh = getemptyobject(doc, "Mesh::Feature", "genwingmesh")
 mesh.Mesh = Mesh.Mesh(genfacets(12, 30))
---
+++ wingflattening/freecad_macro_work/p7modules/p7wingeval.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+# Macro to make project7wing as list of wires and single bspline surface
+
+import FreeCAD as App
+import Draft, Part, Mesh
+import DraftGeomUtils
+import math, os, csv
+import numpy
+from FreeCAD import Vector, Rotation
+
+doc = App.ActiveDocument
+	
+
+# parametric definition functions for the wingshape
+def paramintconv(u, uvals):
+	j0, j1 = 0, len(uvals)-1
+	while j1 - j0 >= 2:
+		j = (j1 + j0)//2
+		if u <= uvals[j]:
+			j1 = j
+		else:
+			j0 = j
+	return j0 + (u-uvals[j0])/(uvals[j1]-uvals[j0])
+
+def seval(u, v):
+	uc = paramintconv(u, uvals)
+	i = max(0, min(len(uvals)-2, int(uc)))
+	m = uc - i
+	p0 = sections[i].Shape.valueAt(v)
+	p1 = sections[i+1].Shape.valueAt(v)
+	return p0*(1-m) + p1*m
+
+# Fetch the sections from the folder and find the parameter ranges
+sections = doc.getObject("SectionGroup").OutList
+
+leadingedgepoints = [ s.Shape.valueAt(0)  for s in sections ]
+leadingedgelengths = [ 0.0 ]
+for i in range(len(leadingedgepoints)-1):
+	leadingedgelengths.append(leadingedgelengths[-1] + (leadingedgepoints[i+1] - leadingedgepoints[i]).Length)
+uvals = leadingedgelengths
+
+urange = [0, uvals[-1]]
+vrange = [sections[0].Shape.FirstParameter, sections[0].Shape.LastParameter]
+
+print("Ranges", urange, vrange)
+
